7c885c8108bbbf8db4902fa7c6adfe311f372bad
andreatessaro <andreatessaro@jeniadev028>
2020-03-21 10:02:46 +0100

    phemex puzzle solution

---
+++ src/main/java/BitcoinAddress.java
@@ -0,0 +1,51 @@
+import java.math.BigInteger;
+
+import org.bitcoinj.core.Base58;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.params.MainNetParams;
+
+public class BitcoinAddress {
+
+	public static void main(String[] args) throws Exception {
+
+		BigInteger eprime = new BigInteger("957496696762772407663");
+		System.out.println("eprime hex " + eprime.toString());
+
+		BigInteger satoshiNakamoto = new BigInteger(reverseHex(bytesToHex(Base58.decode("SatoshiNakamoto"))), 16);
+		System.out.println("SatoshiNakamoto  " + satoshiNakamoto);
+
+		BigInteger phemex = new BigInteger(reverseHex(bytesToHex(Base58Xrp.decode("Phemex"))), 16);
+		System.out.println("Phemex  " + phemex);
+
+		BigInteger calculation = eprime.multiply(satoshiNakamoto).multiply(phemex);
+		System.out.println("Calculation  " + calculation);
+		System.out.println("CalculationHex  " + calculation.toString(16));
+		
+		ECKey ac = ECKey.fromPrivate(calculation, true);
+		System.out.println(ac.toAddress(MainNetParams.get()));
+
+	}
+
+	public static String reverseHex(String originalHex) {
+		// TODO: Validation that the length is even
+		int lengthInBytes = originalHex.length() / 2;
+		char[] chars = new char[lengthInBytes * 2];
+		for (int index = 0; index < lengthInBytes; index++) {
+			int reversedIndex = lengthInBytes - 1 - index;
+			chars[reversedIndex * 2] = originalHex.charAt(index * 2);
+			chars[reversedIndex * 2 + 1] = originalHex.charAt(index * 2 + 1);
+		}
+		return new String(chars);
+	}
+
+	private static String bytesToHex(byte[] hashInBytes) {
+
+		StringBuilder sb = new StringBuilder();
+		for (byte b : hashInBytes) {
+			sb.append(String.format("%02x", b));
+		}
+		return sb.toString();
+
+	}
+
+}
